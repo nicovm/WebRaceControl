ca784d470f4d896dae04c638e86f194120c9d169
pg2 pg2. Programador2 <pg2@aumax.local>
2017-01-27 21:32:54 -0300

    Inicio

---
+++ RaceControl/Controllers/TorneoController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RaceControl.Models;
+
+namespace RaceControl.Content
+{
+    public class TorneoController : Controller
+    {
+        private RaceControlEntities db = new RaceControlEntities();
+
+        // GET: Torneos
+        public ActionResult Index()
+        {
+            return View(db.Torneo.ToList());
+        }
+
+        // GET: Torneos/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Torneo torneo = db.Torneo.Find(id);
+            if (torneo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(torneo);
+        }
+
+        // GET: Torneos/Create
+        public ActionResult Create()
+        {
+            return PartialView("_Torneo");
+        }
+
+        // POST: Torneos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idTorneo,nombre,fecha,idCliente")] Torneo torneo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Torneo.Add(torneo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(torneo);
+        }
+
+        // GET: Torneos/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Torneo torneo = db.Torneo.Find(id);
+            if (torneo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(torneo);
+        }
+
+        // POST: Torneos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idTorneo,nombre,fecha,idCliente")] Torneo torneo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(torneo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(torneo);
+        }
+
+        // GET: Torneos/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Torneo torneo = db.Torneo.Find(id);
+            if (torneo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(torneo);
+        }
+
+        // POST: Torneos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Torneo torneo = db.Torneo.Find(id);
+            db.Torneo.Remove(torneo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        // GET: /Torneo/GetOne/5
+        public ActionResult GetOne(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Torneo torneo = db.Torneo.Find(id);
+            if (torneo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(torneo);
+        }
+    }
+}
---
+++ RaceControl/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RaceControl.Startup))]
+namespace RaceControl
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
