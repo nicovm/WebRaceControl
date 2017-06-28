6ad82ce0fd5f471e6c6491b8b1e371054222b05b
NFB <nicolasb@aumax.com.ar>
2017-06-27 21:11:02 -0300

    Se avanzo en el proyecto

--- RaceControl/App_Start/FilterConfig.cs
+++ RaceControl/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace RaceControl
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
