92554cc8e42d52fc4605dda25d98cf806464e73d
NFB <nicolasb@aumax.com.ar>
2017-07-06 09:46:17 -0300

    Se avanzo en el diseño de la apliacacion

--- RaceControl/Controllers/HomeController.cs
+++ RaceControl/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RaceControl.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,11 @@ namespace RaceControl.Controllers
 {
     public class HomeController : Controller
     {
+        private EntidadesRaceControl db = new EntidadesRaceControl();
+
         public ActionResult Index()
         {
+            ViewBag.torneos = db.Torneo.ToList();
             return View();
         }
 
